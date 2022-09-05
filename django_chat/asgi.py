f44dbf0e8d683262247df6e088edd59833e83139
kamandkrg <kamandkargar@gmail.com>
2022-09-05 14:43:26 +0430

    add consumers and install channels

--- django_chat/asgi.py
+++ django_chat/asgi.py
@@ -8,14 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)))
 })
