3cc035af6bec2d9bb8642f66ae8cbc9362591e62
kamandkrg <kamandkargar@gmail.com>
2022-09-19 09:34:05 +0430

    fix bug in recieve websocket

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,13 +2,14 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.db import database_sync_to_async
+from channels.exceptions import StopConsumer
 from channels.generic.websocket import AsyncConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer, WebsocketConsumer
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 
 from account.models import User
-from chat.models import PrivateChatRoom, Message
+from chat.models import PrivateChatRoom, Message, StatusMessage
 
 
 class ChatConsumer(AsyncConsumer):
@@ -16,7 +17,7 @@ class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         self.user = self.scope['user']
         user2 = self.scope['url_route']['kwargs']['username']
-        self.chat = await self.get_chat(self.user, user2)
+        self.chat = await self.get_chat(self.user.username, user2)
         self.chat_id = f'chat_{self.chat.pk}'
 
         await self.channel_layer.group_add(
@@ -32,41 +33,49 @@ class ChatConsumer(AsyncConsumer):
             self.chat_id,
             self.channel_name
         )
+        raise StopConsumer()
 
-    async def websocket_receive(self, text_data=None, bytes_data=None):
+    async def websocket_receive(self, event):
+        text_data = event.get('text', None)
+        bytes_data = event.get('bytes', None)
+
         if text_data is not None:
-            text_data = json.dumps(text_data)
+            print(text_data, 222222222222222222)
             text_data_json = json.loads(text_data)
-            message = json.loads(text_data_json['text'])
+            text = text_data_json['text']
             user = self.scope['user']
             username = 'default'
             if user.is_authenticated:
                 username = user.username
-            msg = json.dumps({'type': 'msg', 'message': message, 'username': username})
+            msg = json.dumps({'type': 'msg', 'text': text, 'username': username})
 
-            await self.create_message(message)
+            await self.create_message(text)
             await self.channel_layer.group_send(
             self.chat_id, {
                 'type': 'chat_message',
                 'message': msg,
+                'sender_channel_name': self.channel_name
                 }
             )
 
     async def chat_message(self, event):
-        await self.send({
-            'type': 'websocket.send',
-            'text': event['message']
-        })
+        if self.channel_name != event['sender_channel_name']:
+            await self.send({
+                'type': 'websocket.send',
+                'text': event['message']
+            })
 
     @database_sync_to_async
     def create_message(self, text):
         room = get_object_or_404(PrivateChatRoom, pk=self.chat.pk)
-        message = Message(auther=self.scope['user'].username, text=text['text'], room=room)
-        message.save()
+        message = Message.objects.create(auther=self.scope['user'], text=text, room=room)
 
     @database_sync_to_async
     def get_chat(self, user, user2):
+        user1 = get_object_or_404(User, username=user)
+        user_2 = get_object_or_404(User, username=user2)
         chat = PrivateChatRoom.objects.filter(
-            Q(user1=user.username, user2=user2) | Q(user2=user.username, user1=user2)
+            Q(user1=user1, user2=user_2) | Q(user2=user1, user1=user_2)
         ).first()
+
         return chat
